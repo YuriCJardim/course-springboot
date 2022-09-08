ba13ad84dbc4cc6a203e20031b2097e7b9782bcf
YuriCJardim <yuricjardim@gmail.com>
2022-09-08 12:32:00 -0300

    GetMapping

--- src/main/java/br/com/yuricorrea/course/entities/User.java
+++ src/main/java/br/com/yuricorrea/course/entities/User.java
@@ -1,2 +1,57 @@
-package br.com.yuricorrea.course.entities;public class User {
+package br.com.yuricorrea.course.entities;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+    private long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String password;
+
+    public  User (){}
+
+    public User(long id, String name, String email, String phone, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+
+        User user = (User) o;
+
+        return getId() == user.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (getId() ^ (getId() >>> 32));
+    }
 }
--- src/main/java/br/com/yuricorrea/course/resources/UserResource.java
+++ src/main/java/br/com/yuricorrea/course/resources/UserResource.java
@@ -1,2 +1,21 @@
-package br.com.yuricorrea.course.resources;public class UserResource {
+package br.com.yuricorrea.course.resources;
+
+import br.com.yuricorrea.course.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping (value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+
+        User user = new User(1L,"Maria", "maria@gmail.com", "9999999", "12345");
+        return ResponseEntity.ok().body(user);
+
+    }
+
 }
