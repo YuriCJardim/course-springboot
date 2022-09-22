7359c127e3f39055505f140fbb9367a54c625415
YuriCJardim <yuricjardim@gmail.com>
2022-09-21 23:56:55 -0300

    Exception handling - update

--- src/main/java/br/com/yuricorrea/course/services/UserService.java
+++ src/main/java/br/com/yuricorrea/course/services/UserService.java
@@ -10,6 +10,7 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 
@@ -44,9 +45,13 @@ public class UserService {
     }
 
     public User update (long id, User obj){
-        User entity = userRepository.getOne(id);
-        updateData(entity, obj);
-        return userRepository.save(entity);
+        try {
+            User entity = userRepository.getOne(id);
+            updateData(entity, obj);
+            return userRepository.save(entity);
+        }catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
